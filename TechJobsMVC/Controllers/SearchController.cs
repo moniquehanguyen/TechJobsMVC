9f6fe36657d3944438612bd1145b0161452332cf
Monique Nguyen <moniquehanguyen@gmail.com>
2020-12-17 16:46:26 -0600

    Completed updating List controller and view, and Search controller and view.

--- TechJobsMVC/Controllers/SearchController.cs
+++ TechJobsMVC/Controllers/SearchController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using TechJobsMVC.Data;
+using TechJobsMVC.Models;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -14,9 +16,36 @@ namespace TechJobsMVC.Controllers
         public IActionResult Index()
         {
             ViewBag.columns = ListController.ColumnChoices;
+            ViewBag.tableChoices = ListController.TableChoices;
+            ViewBag.employers = JobData.GetAllEmployers();
+            ViewBag.locations = JobData.GetAllLocations();
+            ViewBag.positionTypes = JobData.GetAllPositionTypes();
+            ViewBag.skills = JobData.GetAllCoreCompetencies();
+
             return View();
         }
 
         // TODO #3: Create an action method to process a search request and render the updated search view. 
+        public IActionResult Results(string searchType, string searchTerm)
+        {
+            ViewBag.searchType = searchType;
+            ViewBag.searchTerm = searchTerm;
+            List<Job> jobs;
+            
+            if (searchTerm == null)
+            {
+                jobs = JobData.FindAll();
+                ViewBag.title = "All Jobs";
+            }
+            else
+            {
+                jobs = JobData.FindByColumnAndValue(searchType, searchTerm);
+                ViewBag.title = "Searching " + searchTerm.ToUpper() + " in category " + searchType.ToUpper();
+            }
+
+            ViewBag.jobs = jobs;
+            ViewBag.columns = ListController.ColumnChoices;
+            return View("Index");
+        }
     }
 }
